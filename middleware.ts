18d01591647d24c6d57ec6f42b5b4d33b82ad4da
Rodrigo Calle <eate.rodrigogustavo.calle.co@unifranz.edu.bo>
2026-06-15 23:38:17 -0400

    fix: middleware en todas las rutas para sesion en produccion

--- middleware.ts
+++ middleware.ts
@@ -38,5 +38,7 @@ export async function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: ['/dashboard/:path*', '/book/:path*'],
+  matcher: [
+    '/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)',
+  ],
 }
